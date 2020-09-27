e1a5bd6538c1e03c8229034e15431ce229cfa92b
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-09-27 11:33:56 +0300

    start

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+
+const cors = require('cors');
+
+const app = express();
+
+// Basic Configuration 
+const port = process.env.PORT || 3000;
+
+/** this project needs a db !! **/ 
+// mongoose.connect(process.env.DB_URI);
+
+app.use(cors());
+
+/** this project needs to parse POST bodies **/
+// you should mount the body-parser here
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+  
+// your first API endpoint... 
+app.get("/api/hello", (req, res) => {
+  res.json({greeting: 'hello API'});
+});
+
+
+app.listen(port, () => {
+  console.log('Node.js listening ...', port);
+});
